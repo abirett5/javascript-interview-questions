21f3dac721bcb6e3b449f697489516636c994573
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-07-14 18:08:26 +0545

    Updated

--- ques_1.js
+++ ques_1.js
@@ -1,1 +1,12 @@
+function abc() {
+    console.log(a);
 
+    // let & const will be hoisted as well, but they are going to the temporal dead zone
+    const c = 30;
+    let b = 20;
+
+    // var will be hoisted completely
+    var a = 10;
+}
+
+abc();
